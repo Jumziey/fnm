f9a55a70a51e4929ae99c4677a9197d407a93b25
Jumziey <jumziey@gmail.com>
2015-02-02 09:58:09 +0100

    Started building initial conditions

--- lab1/ex1/fft.c
+++ lab1/ex1/fft.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
-#include <gsl/gsl_matrix.h>
-int main(){
- gsl_matrix * M;
- M = gsl_matrix_calloc (10, 12);
- gsl_matrix_set (M, 1, 3, 5.);
- gsl_matrix_fprintf (stdout, M, "%e");
- return 0;
+#include <stdlib.h>
+#include <math.h>
+//#include <gsl/gsl_matrix.h>
+
+//Damn c99!!!
+#ifndef M_PI
+#define M_PI           3.14159265358979323846
+#endif
+#define SIZE 1024
+
+double dt = 1/SIZE;
+
+void
+printArray(double* grid, int size) {
+	int i;
+	
+	for(i=0; i<size; i++)
+		printf("%f\n",grid[i]);
 }
+
+int main(){
+	int i;
+	double grid[SIZE];
+	long double alpha = 16*dt;
+	printf("alpha: %.4f\n",alpha);
+	long double exponent;
+	long double c = 1/(sqrt(M_PI*alpha*alpha));
+	printf("const: %g\n",c);
+	return 0;
+	for(i=0; i<SIZE; i++) {
+		exponent = i*i*dt*dt;
+		
+		printf("%g\n",exp(-exponent));
+	}
+}
+
