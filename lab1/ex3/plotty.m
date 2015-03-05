9dcd5b90db7139af5d55e0a6e7c502b0e44f5321
Jesper Vesterberg <jumziey@gmail.com>
2015-03-05 16:13:33 +0100

    Done with lab 1

--- lab1/ex3/plotty.m
+++ lab1/ex3/plotty.m
@@ -28,18 +28,21 @@ title('Filtered Frequency Power Spectrum')
 xlabel('Frequency (Hz)')
 ylabel('Magnitude')
 %axis([0 4500 0 5e6])
+matlab2tikz('freqPowSpec.tikz', 'height', '\figureheight', 'width', '\figurewidth');
 
 figure(2)
+subplot(2,1,2)
 filtered = load('filteredSignal');
 plot(0:1/N:1-1/N,filtered.^2)
 title('Filtered Signal')
 xlabel('time (s)')
 ylabel('Value')
 
-figure(3)
+subplot(2,1,1)
 init = load('am26.dat');
 plot(0:1/N:1-1/N,init.^2);
 title('Original Signal')
 xlabel('time (s)')
 ylabel('Value')
+matlab2tikz('signal.tikz', 'height', '\figureheight', 'width', '\figurewidth');
 
