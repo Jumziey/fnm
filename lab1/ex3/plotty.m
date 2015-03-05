791632d29342262d4833813f5fca11943e171fe1
Jesper Vesterberg <jumziey@gmail.com>
2015-03-05 15:26:50 +0100

    Removed some files

--- lab1/ex3/plotty.m
+++ lab1/ex3/plotty.m
@@ -1,75 +1,45 @@
 clear all; close all;
 
-N = 8192;
-
-orig = load('am26.dat');
-%origReal = orig(1:2:N*2);
-%origIm = orig(2:2:N*2);
-%orig = origReal.^2.*origIm.^2;
 realTrans = load('realTrans');
-imTrans = load('imTrans');
+imagTrans = load('imagTrans');
+trans = (realTrans.^2 + imagTrans.^2);
+realFilteredTrans = load('realFilteredTrans');
+imagFilteredTrans = load('imagFilteredTrans');
+fTrans = (realFilteredTrans.^2 + imagFilteredTrans.^2);
 
-t = linspace(0,1,N) ;
-%Plot how it looks initially
+N = 8192;
+df = 1/(N);
+
+trans = [trans(N/2+1:N-1);0;trans(1:N/2-1);0];
+fTrans = [fTrans(N/2+1:N-1);0;fTrans(1:N/2-1);0];
+f = linspace(-1/(2*df), 1/(2*df), N+1);
+f = f(1:end-1);
+
+figure(1)
 subplot(2,1,1)
-
-plot(t,orig);
-xlabel('Time (s)');
-ylabel('Value');
-title('Noisy signal')
-
+plot(f,trans)
+title('Frequency Power Spectrum')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+%axis([0 4500 0 5e6])
 subplot(2,1,2)
-plot(t,orig.^2);
-xlabel('Time (s)');
-ylabel('Value');
-title('Noisy signal squared')
-
+plot(f,fTrans)
+title('Filtered Frequency Power Spectrum')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+%axis([0 4500 0 5e6])
 
 figure(2)
-subplot(2,1,1)
-%K vector...? lets see we have n = linspace(-N/2,N/2,N), N = 1024
-% Then fn = n/(dt*N);
-
-%Much simpler way to put it is that we go from minimum nyquist freq to cax. with N values all and all. 
-%Now the minus frequency is the same as the positive counterpart
-trans = (realTrans.^2 + imTrans.^2);
-dt = 1/N;
-f = linspace(-1/(2*dt),1/(2*dt), N);
-f = linspace(0,1/(N*dt), N/2)
-f = [f f];
-%trans goes from 0 to 1/(dt*2), -1/(dt*2) to 0, due to fft implementation.
-origTrans = trans;
-%trans = trans - min(trans); %Remove offset 
-%trans = trans./max(trans); %Normalize
-
-plot(f,trans.*trans)
-hold on
-%plot([0 0],[0 600], 'r')
-xlabel('Frequency (Hz)');
-ylabel('Power Magnitude (normalized)');
-title('Fourier Transform of Signal Data ')
-
-%Filter
-fc = 1280;
-bandwidth = 1792-767.6;
-filter = exp( (-1/2)* ( (abs(f)-fc)./bandwidth).^2);
-subplot(2,1,2)
-plot(f,(filter'.*trans).^2)
-title('Filtered Fourier Transform 1');
-
-
-
-
-%Filtered Signal 
-figure(3)
-subplot(2,1,1)
 filtered = load('filteredSignal');
-plot(t,filtered);
-subplot(2,1,2)
-plot(t,filtered.^2)
-filtered - orig
+plot(0:1/N:1-1/N,filtered.^2)
+title('Filtered Signal')
+xlabel('time (s)')
+ylabel('Value')
 
-figure(4)
-FF = load('FilteredFourier');
-FF = [FF(N/2+1:N); FF(2:N/2); 0]
-plot(f,FF.^2)
+figure(3)
+init = load('am26.dat');
+plot(0:1/N:1-1/N,init.^2);
+title('Original Signal')
+xlabel('time (s)')
+ylabel('Value')
+
