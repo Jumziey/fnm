58a4b0351a39f7eb171d8e5cddf86d5123640cba
Jesper Vesterberg <jumziey@gmail.com>
2015-03-05 13:01:03 +0100

    added some files

---
+++ lab1/ex3/plotty2.m
@@ -0,0 +1,45 @@
+clear all; close all;
+
+realTrans = load('realTrans');
+imagTrans = load('imagTrans');
+trans = (realTrans.^2 + imagTrans.^2);
+realFilteredTrans = load('realFilteredTrans');
+imagFilteredTrans = load('imagFilteredTrans');
+fTrans = (realFilteredTrans.^2 + imagFilteredTrans.^2);
+
+N = 8192;
+df = 1/(N);
+
+trans = [trans(N/2+1:N-1);0;trans(1:N/2-1);0];
+fTrans = [fTrans(N/2+1:N-1);0;fTrans(1:N/2-1);0];
+f = linspace(-1/(2*df), 1/(2*df), N+1);
+f = f(1:end-1);
+
+figure(1)
+subplot(2,1,1)
+plot(f,trans)
+title('Frequency Power Spectrum')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+%axis([0 4500 0 5e6])
+subplot(2,1,2)
+plot(f,fTrans)
+title('Filtered Frequency Power Spectrum')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+%axis([0 4500 0 5e6])
+
+figure(2)
+filtered = load('filteredSignal');
+plot(0:1/N:1-1/N,filtered.^2)
+title('Filtered Signal')
+xlabel('time (s)')
+ylabel('Value')
+
+figure(3)
+init = load('am26.dat');
+plot(0:1/N:1-1/N,init.^2);
+title('Original Signal')
+xlabel('time (s)')
+ylabel('Value')
+
