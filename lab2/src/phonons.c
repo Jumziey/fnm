a04b5faf0aa8aaa3d4f3f95744fafb39b1ee67ab
Jesper Vesterberg <jumziey@gmail.com>
2015-03-10 16:07:09 +0100

    Test program is broken... W8ing for fix

--- lab2/src/phonons.c
+++ lab2/src/phonons.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "frequencies.c"
+
+//Need to declare
+void frequencies(double A, double B, double m, double *q,
+				double *omega, double *eps);
 
 //Substance Properties
 typedef struct sp{
@@ -25,25 +28,21 @@ error(char* err) {
 }
 
 void
-printOmegaVal(double *q, double *omega) {
+printVal(double *q, double *val) {
 	int i;
 	for(i=0; i<3; i++)
 		printf("%g ", q[i]);
 	for(i=0; i<3; i++)
-		printf("%g ", omega[i]);
+		printf("%g ", val[i]);
 	printf("\n");
 }
 
 
 
 void
-pointEval(sp sub, double* q)
+freqEval(sp sub, double* q)
 {
-	double A,B, *pdir, *omega;
-	
-	//Initialize to zero for frequencies()
-	pdir = calloc(3*3,sizeof(double));
-	omega = calloc(3,sizeof(double));
+	double A,B, pdir[9], omega[3];
 	
 	//Calculating constants A and B from the problem specification
 	A = sub.eps*( 156.0*(pow(sub.sigma,12)/pow(sub.rnn,14)) 
@@ -52,18 +51,14 @@ pointEval(sp sub, double* q)
 	
 	frequencies(A, B, sub.m, q, omega, pdir);
 	
-	printOmegaVal(q,omega);
-	free(pdir);
-	free(omega);
+	printVal(q,omega);
 }
 
 void
-nPointsEval(sp sub, double* q1, double *q2, int n)
+nFreqEval(sp sub, double* q1, double *q2, int n)
 {
 	int i,j;
-	double* diff, *qCur;
-	diff = malloc(3*sizeof(double));
-	qCur = malloc(3*sizeof(double));
+	double diff[3], qCur[3];
 	
 	for(i=0; i<3; i++)
 		diff[i] = (q2[i]-q1[i])/((double)n-1.0);
@@ -71,13 +66,56 @@ nPointsEval(sp sub, double* q1, double *q2, int n)
 	for(i=0; i<n; i++) {
 		for(j=0; j<3; j++)
 			qCur[j] = q1[j]+diff[j]*i;
-		pointEval(sub, qCur);
+		freqEval(sub, qCur);
 	}
-	free(diff);
-	free(qCur);
+}
+
+void
+volDepEval(sp sub, double *q)
+{
+	int i;
+	//minus,normal, plus
+	double omega[3][3], pdir[3][9], rnn[3], gamma[3], A[3], B[3];
+	double h = 1E-20;// small compared to rnn
+	
+	//Initialize rnn values early for easy debug
+	for(i=-1; i<2; i++)
+		rnn[i+1] = sub.rnn + h*i;
+	
+	//We only have rnn dependency in A and B
+	//(By extension only volume dependence here)
+	for(i=0; i<3; i++){
+		A[i] = sub.eps*( 156.0*(pow(sub.sigma,12)/pow(rnn[i],14)) 
+					- 84.0*(pow(sub.sigma,6)/pow(rnn[i],8)) );
+		B[i] = 12.0* ( ( ( pow(sub.sigma,6)*(pow(rnn[i],6)-pow(sub.sigma,6)) )/pow(rnn[i],14) ) *sub.eps);
+	}
+	
+	for(i=0; i<3; i++) 
+		frequencies(A[i], B[i], sub.m, q, omega[i], pdir[i]);
+	
+	//Array order is minus, normal, plus
+	for(i=0; i<3; i++) {
+		gamma[i] = (omega[2][i] - omega[0][i])/(pow(rnn[2],3)-pow(rnn[0],3));
+		gamma[i] = fabs(gamma[i]*pow(rnn[1],3)/omega[1][i]);
+	}
+	printVal(q, gamma);
 }
 		
-
+void
+nValDepEval(sp sub, double *q1, double *q2, int n)
+{
+	int i,j;
+	double diff[3], qCur[3];
+	
+	for(i=0; i<3; i++)
+		diff[i] = (q2[i]-q1[i])/((double)n-1.0);
+		
+	for(i=0; i<n; i++) {
+		for(j=0; j<3; j++)
+			qCur[j] = q1[j]+diff[j]*i;
+		volDepEval(sub, qCur);
+	}
+}
 
 
 int
@@ -112,11 +150,9 @@ int
 main(int argc, char** argv)
 {
 	int mode, subindex, npoints;
-	double *q1, *q2;
+	double q1[3], q2[3];
 	//Index ordering is coupled with subInd()
 	const sp subs[4] = {Ne, Ar, Kr, Xe};
-
-	
 		
 	if((subindex = subInd(argv[1]))<0)
 		error("Unkown (for the program) substance\n");
@@ -124,37 +160,32 @@ main(int argc, char** argv)
 	if((mode = operMode(argv[2]))<0)
 		error("Mode is not implemented\n");
 	
-	
+	int k;
 	//Dealing with the command line arguments
 	switch(mode) {
 		case 0: //Omega
 			switch(argc) {
 				case 6:
-					q1 = malloc(sizeof(double)*3);
+					//We only give frequency for one point
 					q1[0] = atof(argv[3]); q1[1] = atof(argv[4]); q1[2] = atof(argv[5]);
-					//printf("only one point\n");
-					pointEval(subs[subindex], q1);
-					free(q1);
+					freqEval(subs[subindex], q1);
 					break;
 				case 9:
-					q1 = malloc(sizeof(double)*3);
-					q2 = malloc(sizeof(double)*3);
+					//Two points, frequency need to be evaluated in 11 points
+					//Equally spaced
 					q1[0] = atof(argv[3]); q1[1] = atof(argv[4]); q1[2] = atof(argv[5]);
 					q2[0] = atof(argv[6]); q2[1] = atof(argv[7]); q2[2] = atof(argv[8]);
-					nPointsEval(subs[subindex], q1, q2, 11);
-					//Two points, need to be spaced 11
-					free(q1);free(q2);
+					nFreqEval(subs[subindex], q1, q2, 11);
 					break;
 				case 10:
+					//Two points, frequency needs to evaluated in <npoints> points
+					//Equally spaced
 					npoints = atof(argv[9]);
 					if(npoints<2)
 						error("Need to evaluate two or more points if a range is given\n");
-					q1 = malloc(sizeof(double)*3);
-					q2 = malloc(sizeof(double)*3);
 					q1[0] = atof(argv[3]); q1[1] = atof(argv[4]); q1[2] = atof(argv[5]);
 					q2[0] = atof(argv[6]); q2[1] = atof(argv[7]); q2[2] = atof(argv[8]);
-					nPointsEval(subs[subindex], q1, q2, npoints);
-					free(q1);free(q2);
+					nFreqEval(subs[subindex], q1, q2, npoints);
 					break;
 				default:
 					error("Wrong number of arguments\n");
@@ -163,11 +194,15 @@ main(int argc, char** argv)
 		case 1: // gamma
 			switch(argc) {
 				case 6:
-					printf("CRAZY!\n");
+					//Calculating volume dep. of phonon frequency in one point
+					q1[0] = atof(argv[3]); q1[1] = atof(argv[4]); q1[2] = atof(argv[5]);
+					volDepEval(subs[subindex], q1);
 					break;
 				case 9:
-					printf("blaoeuhtn\n");
 					//Two points, need to be spaced 11
+					q1[0] = atof(argv[3]); q1[1] = atof(argv[4]); q1[2] = atof(argv[5]);
+					q2[0] = atof(argv[6]); q2[1] = atof(argv[7]); q2[2] = atof(argv[8]);
+					nValDepEval(subs[subindex], q1, q2, 11);
 					break;
 				case 10:
 					//Two points and npoints
@@ -181,10 +216,10 @@ main(int argc, char** argv)
 		case 2: // cv
 			switch(argc) {
 				case 4:
-					printf("CRAZY!\n");
+					//printf("CRAZY!\n");
 					break;
 				case 5:
-					printf("blaoeuhtn\n");
+					//printf("blaoeuhtn\n");
 					//Two points, need to be spaced 11
 					break;
 				case 6:
@@ -197,12 +232,9 @@ main(int argc, char** argv)
 			}
 			break;
 		default:
-			printf("CongratZ! You've reach an unreachable part of the program\n");
+			k=0;
+			//printf("CongratZ! You've reach what the programmer thought to be an unreachable part of the program\n");		
 	}
 	
-	
-	
-
-
 	return 0;	
 }
