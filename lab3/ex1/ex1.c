3b8acac67df2de651f46ef8a165ed77fd51997de
Jesper Vesterberg <jumziey@gmail.com>
2015-03-12 16:59:38 +0100

    started on report

--- lab3/ex1/ex1.c
+++ lab3/ex1/ex1.c
@@ -8,10 +8,25 @@ const double epsrel = 1E-12;
 const double epsabs = 1E-8;
 
 double
-cyl(double x, void* params)
+top(double x, void* params)
 {
-	return top_y(x)*top_y(x)*top_rho(x);
+	return M_PI*top_y(x)*top_y(x)*top_rho(x);
 }
+
+void
+saveData(double (*func)(double), char* file, double s, double e, int n)
+{
+	int i;
+	double diff;
+	FILE* fp;
+	
+	fp = fopen(file, "w");
+	diff = (e-s)/(double)n;
+	for(i=0; i<n; i++)
+		fprintf(fp,"%g %g\n", s+diff*i, func(s+diff*i));
+	fclose(fp);
+}
+	
 int
 main()
 {
@@ -19,14 +34,12 @@ main()
 	size_t limit;
 	gsl_integration_workspace *w;
 	gsl_function F;
-	F.function = &cyl;
+	F.function = &top;
 	F.params = NULL;
 
 	limit = 1000;
 	w = gsl_integration_workspace_alloc(limit);
 	gsl_integration_qags(&F, 0, 5, epsabs, epsrel, limit, w, &result, &abserr);
-	
-	printf("result: %g\n", result);
 	
 	return 0;
 	
