d11e60db528d46d99a5bc37104efe641ff09ad34
Jesper Vesterberg <jeve0010@dlab072.(none)>
2015-03-24 16:54:59 +0100

    Getting AHEAD OF MYSELF

---
+++ lab3/ex2/top.h
@@ -0,0 +1,15 @@
+/* Function for the definition of a symmetric top 
+   
+   Given the distance z (in cm) to the tip of the top along the principle 
+   axis of symmetry,
+   
+   top_y returns the distance to the principle axis of symmetry
+   
+   top_rho returns the mass density 
+
+*/
+
+double top_y(double z);
+
+double top_rho(double z);
+
