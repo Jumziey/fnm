8ba2172311e4363d22c6835105880ccc2dc10a68
Jesper Vesterberg <jumziey@gmail.com>
2015-03-26 09:46:54 +0100

    tjoho

--- lab3/ex3/ex3.c
+++ lab3/ex3/ex3.c
@@ -15,22 +15,6 @@ const double epsabs = 1E-8;
 //Yes i could use params... But LAZY! :D
 double M,l,g,i1,i3;
 
-
-//For reverse engineering
-void
-saveData(double (*func)(double), char* file, double s, double e, int n)
-{
-	int i;
-	double diff;
-	FILE* fp;
-	
-	fp = fopen(file, "w");
-	diff = (e-s)/(double)n;
-	for(i=0; i<n; i++)
-		fprintf(fp,"%g %g\n", s+diff*i, func(s+diff*i));
-	fclose(fp);
-}
-
 //Integral expressions
 double
 mFunc(double x, void* params)
@@ -64,17 +48,32 @@ topHam(double time, const double v[], double d[], void *params)
 {
 	const double c = cos(v[2]);
 	const double s = sin(v[2]);
-	
-	d[0] = (2*v[3] - 2*v[4]*c)/(2*i1*s*s);
-	d[1] = v[4]/i3 - (c*(v[3] - v[4]*c))/(i1*s*s);
-	d[2] = v[5]/i1;
+	double phi,psi,theta,pphi,ppsi,ptheta,I1,I3;
+	phi = v[0]; psi=v[1];theta=v[2];pphi=v[3];ppsi=v[4];ptheta=v[5];
+	I1=i1;I3=i3;
 	
 	d[3] = 0;
 	d[4] = 0;
-	d[5] = M*l*g*s - (v[4]*(v[3] - v[4]*c))/(i1*s) + (c*pow((v[3] - v[4]*c),2))/(i1*s*s*s);
+	d[5] = M*l*g*sin(theta) - (ppsi*(pphi - ppsi*cos(theta)))/(I1*sin(theta)) + (cos(theta)*pow((pphi - ppsi*cos(theta)),2))/(I1*pow(sin(theta),3));
+	
+	d[0] = (2.0*pphi - 2.0*ppsi*cos(theta))/(2.0*I1*pow(sin(theta),2));
+	d[1] = ppsi/I3 - (cos(theta)*(pphi - ppsi*cos(theta)))/(I1*pow(sin(theta),2));
+	d[2] = ptheta/I1;
+	
+	
 	
 	return GSL_SUCCESS;
 }
+
+/*
+phi_dot = (2*pphi - 2*ppsi*cos(theta))/(2*I1*sin(theta)^2)
+psi_dot = ppsi/I3 - (cos(theta)*(pphi - ppsi*cos(theta)))/(I1*sin(theta)^2)
+theta_dot = ptheta/I1
+pphi_dot =0
+ppsi_dot =0
+ptheta_dot =Mlg*sin(theta) - (ppsi*(pphi - ppsi*cos(theta)))/(I1*sin(theta)) + (cos(theta)*(pphi - ppsi*cos(theta))^2)/(I1*sin(theta)^3)
+ */
+
 
 int
 topJac(double time, const double v[], double *dfdv, double *dfdt, void *params)
@@ -91,13 +90,13 @@ topJac(double time, const double v[], double *dfdv, double *dfdt, void *params)
  	
 	gsl_matrix_set(m,0,0,0);
 	gsl_matrix_set(m,0,1,0);
-	gsl_matrix_set(m,0,2,v[4]/(i1*s) - (c*(2*v[3] - 2*v[4]*c))/(i1*pow(s,3)));
+	gsl_matrix_set(m,0,2,v[4]/(i1*s) - (c*(2.0*v[3] - 2.0*v[4]*c))/(i1*pow(s,3)));
 	gsl_matrix_set(m,0,3,1/(i1*s*s));
 	gsl_matrix_set(m,0,4,-c/(i1*s*s));
 	gsl_matrix_set(m,0,5,0);
 	gsl_matrix_set(m,1,0,0);
 	gsl_matrix_set(m,1,1,0);
-	gsl_matrix_set(m,1,2,(v[3] - v[4]*c)/(i1*s) - (v[4]*c)/(i1*s) + (2*c*c*(v[3] - v[4]*c))/(i1*s*s*s));
+	gsl_matrix_set(m,1,2,(v[3] - v[4]*c)/(i1*s) - (v[4]*c)/(i1*s) + (2.0*c*c*(v[3] - v[4]*c))/(i1*s*s*s));
 	gsl_matrix_set(m,1,3,-c/(i1*s*s));
 	gsl_matrix_set(m,1,4,1/i3 + c*s/(i1*s*s));
 	gsl_matrix_set(m,1,5,0);
@@ -121,9 +120,9 @@ topJac(double time, const double v[], double *dfdv, double *dfdt, void *params)
 	gsl_matrix_set(m,4,5,0);
 	gsl_matrix_set(m,5,0,0);
 	gsl_matrix_set(m,5,1,0);
-	gsl_matrix_set(m,5,2,M*l*g*c - v[4]*v[4]/i1 - pow((v[3] - v[4]*c),2)/(i1*s*s) - (3*c*c*pow((v[3] - v[4]*c),2))/(i1*pow(s,4)) + (3*v[4]*c*(v[3] - v[4]*c))/(i1*s*s));
-	gsl_matrix_set(m,5,3,(c*(2*v[3] - 2*v[4]*c))/(i1*s*s*s) - v[4]/(i1*s));
-	gsl_matrix_set(m,5,4,(v[4]*c)/(i1*s) - (v[3] - v[4]*c)/(i1*s) - (2*c*c*(v[3] - v[4]*c))/(i1*s*s*s));
+	gsl_matrix_set(m,5,2,M*l*g*c - v[4]*v[4]/i1 - pow((v[3] - v[4]*c),2)/(i1*s*s) - (3.0*c*c*pow((v[3] - v[4]*c),2))/(i1*pow(s,4)) + (3.0*v[4]*c*(v[3] - v[4]*c))/(i1*s*s));
+	gsl_matrix_set(m,5,3,(c*(2.0*v[3] - 2.0*v[4]*c))/(i1*s*s*s) - v[4]/(i1*s));
+	gsl_matrix_set(m,5,4,(v[4]*c)/(i1*s) - (v[3] - v[4]*c)/(i1*s) - (2.0*c*c*(v[3] - v[4]*c))/(i1*s*s*s));
 	gsl_matrix_set(m,5,5,0);
 	
 	for(i=0;i<6;i++)
@@ -136,7 +135,6 @@ int
 main()
 {
 	int funcs = 4;
-
 
 	int i;
 	double abserr, consts[funcs];
@@ -163,22 +161,16 @@ main()
 	
 	//Now lets do some ode solving
 	int status;
-	double k[2], kDiff[2], energy, eDiff, c, s;
 	double t = 0.0;
 	double tEnd = 4.0;
-	double h = 2e-3;
+	double h = 2e-4;
 	double ti;
 	gsl_odeiv2_system sys = {topHam, topJac, 6, NULL};
 	gsl_odeiv2_driver *d = 
-		gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_bsimp, h,epsabs, epsrel);
+		gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rkf45, h, epsabs, epsrel);
 	
 	//initial values
 	double v[] = {0.0, 0.0, 20.0, 0.0, 10.0, 0.0};
-	k[0] = i1*v[3]*pow(sin(v[2]),2) + i3*(v[4]+v[3]*cos(v[2]))*cos(v[2]);
-	k[1] = i3*(v[4]+v[3]*cos(v[2]));
-
-	s = sin(v[2]);c = cos(v[2]);
-	energy =  (i1/2 * (pow(v[5],2) + pow(v[3]*s,2)) + i3/2 * pow((v[4] + v[3]*c),2) + M*g*l*c);
 	
 	//Running simulation forwards
 	FILE *fp;
@@ -190,32 +182,10 @@ main()
 			fprintf(stderr,"Problems!\n");
 			break;
 		}
-		c = cos(v[2]);s = sin(v[2]);
-		kDiff[0] =k[0]- i1*v[3]*pow(sin(v[2]),2) - i3*(v[4]+v[3]*c)*c;
-		kDiff[1] =k[1]- i3*(v[4]+v[3]*cos(v[2]));
-		eDiff = energy- (i1/2 * (pow(v[5],2) + pow(v[3]*s,2)) + i3/2 * pow((v[4] + v[3]*c),2) + M*g*l*c);
-		fprintf(fp,"%.6f %.6f %.6f \t %g %g %g\n", v[0], v[1], v[2], kDiff[0], kDiff[1], eDiff);
+		fprintf(fp,"%.6f %.6f %.6f\n", v[0], v[1], v[2]);
 	}
+	printf("t: %f\n",t);
 	fclose(fp);
-	
-	//Running simulation backwards
-	gsl_odeiv2_driver_free(d);
-	d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rkf45, -h,epsabs, epsrel);
-	tEnd = 0;
-	fp = fopen("./plots/data/backwardData","w");
-	while(t>0) {
-		ti = t-h;
-		status = gsl_odeiv2_driver_apply(d, &t, ti, v);
-		if(status != GSL_SUCCESS){
-			fprintf(stderr,"Problems!\n");
-			break;
-		}
-		c = cos(v[2]);s = sin(v[2]);
-		kDiff[0] =k[0]- i1*v[3]*pow(sin(v[2]),2) - i3*(v[4]+v[3]*c)*c;
-		kDiff[1] =k[1]- i3*(v[4]+v[3]*cos(v[2]));
-		eDiff = energy- (i1/2 * (pow(v[5],2) + pow(v[3]*s,2)) + i3/2 * pow((v[4] + v[3]*c),2) + M*g*l*c);
-		fprintf(fp,"%.6f %.6f %.6f \t %g %g %g\n", v[0], v[1], v[2], kDiff[0], kDiff[1], eDiff);
-	}
-	fclose(fp);
+
 	return 0;
 }
