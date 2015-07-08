1d142008232c23688cabc4a8e2c250dc732f6489
Jesper Vesterberg <jumziey@gmail.com>
2015-07-08 21:13:05 +0200

    changed things around again so correct waveforms will appear

--- lab5/src/runs/potWellk0.sh
+++ lab5/src/runs/potWellk0.sh
@@ -4,9 +4,13 @@ cd ..
 for k in {150..300}
 do
 	runfile="parameters/potWellk0_$k"
-	tail -n +2 parameters/potWellk0 > $runfile
+	cat parameters/potWellk0 > $runfile
 	echo "wf_output_text =wavefunc_potWellk0_$k.dat" >> $runfile
+	
+	kdiff=$(($k-150))
+	nt=$((16000 - $kdiff*100))
+	echo "nt =$nt" >> $runfile
 	command="./potWellWavek0 $runfile -k=$k"
 	echo $command
-	eval $command
+	#eval $command
 done
